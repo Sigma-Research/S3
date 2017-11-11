1e411c04c914a47558017424d9ceda6225d91ae4
wangfengming <wangfengming@hexin.im>
2017-11-11 12:00:41 +0800

    fix resize ratio issue and make resize more smooth

--- s3/processing/views.py
+++ s3/processing/views.py
@@ -11,12 +11,8 @@ import os
 import re
 from django.http import HttpResponse, HttpResponseRedirect
 from s3.constants import STATIC
+
 bucket = 'sigmalove-dev-img'
-
-
-def json_to_response(json_data):
-    response = json.dumps(json_data)
-    return HttpResponse(response, content_type='application/json')
 
 
 def processing(request, name):
@@ -31,65 +27,65 @@ def processing(request, name):
         return HttpResponseRedirect('/%s/%s' % (bucket, name))
 
 
-def resize(w, h, width, height, img):
+def resize(img, size):
+    height, width = img.shape[:2]
+    w, h = size
     w = w if w else int(width * 1.0 / height * h + 0.5)
     h = h if h else int(height * 1.0 / width * w + 0.5)
-    img = cv2.resize(img, (w, h))
-    return img.shape[:2], img
+    return cv2.resize(img, (w, h), interpolation=cv2.INTER_AREA)
 
 
-def rotate(r, img):
-    if r == 90:
-        img = cv2.flip(cv2.transpose(img), 1)
-    elif r == 180:
-        img = cv2.flip(img, -1)
-    elif r == 270:
-        img = cv2.flip(cv2.transpose(img), 0)
+def rotate(img, degree):
+    if degree == 90:
+        return cv2.flip(cv2.transpose(img), 1)
+    elif degree == 180:
+        return cv2.flip(img, -1)
+    elif degree == 270:
+        return cv2.flip(cv2.transpose(img), 0)
     return img
 
 
-def compress(q, ext, img):
-    return cv2.imencode(ext, img, [1, q])[1].tostring()
-
-
-def crop(x, y, w, h, height, width, img):
+def crop(img, rect):
+    height, width = img.shape[:2]
+    x, y, w, h = rect
     w = w or width
     h = h or height
     if x < width and y < height:
-        img = img[y: y + h, x: x + w]
-    return img.shape[:2], img
+        return img[y: y + h, x: x + w]
+    return img
+
+
+def compress(img, ext, quality):
+    return cv2.imencode(ext, img, [1, quality])[1].tostring()
 
 
 def version1(request, object_name, option):
     img = cv2.imread(os.path.join(STATIC, bucket, object_name))
-    height, width = img.shape[:2]
 
-    _, ext = os.path.splitext(option)
-    ext = ext if ext else '.jpg'
     w = re.findall('(\d+)w', option)
     h = re.findall('(\d+)h', option)
-
     w = int(w[-1]) if w else w
     h = int(h[-1]) if h else h
-
     if w or h:
-        (height, width), img = resize(w, h, width, height, img)
+        img = resize(img, (w, h))
 
     a = re.findall('(\d+)-(\d+)-(\d+)-(\d+)a', option)
     if a:
         x, y, w, h = [int(i) for i in a[-1]]
-        img = crop(x, y, w, h, height, width, img)
+        img = crop(img, (x, y, w, h))
 
     r = re.findall('(\d+)r', option)
     if r:
-        r = int(r[-1])
-        img = rotate(r, img)
+        degree = int(r[-1])
+        img = rotate(img, degree)
 
+    _, ext = os.path.splitext(option)
+    ext = ext if ext else '.jpg'
     q = re.findall('(\d+)q', option)
     q = int(q[-1]) if q else 90
 
     try:
-        return HttpResponse(compress(q, ext, img), content_type='image/%s' % (ext[1:]))
+        return HttpResponse(compress(img, ext, q), content_type='image/%s' % (ext[1:]))
     except Exception, e:
         print e
         return HttpResponseRedirect('/%s/%s' % (bucket, object_name))
@@ -97,9 +93,6 @@ def version1(request, object_name, option):
 
 def version2(request, object_name, option):
     img = cv2.imread(os.path.join(STATIC, bucket, object_name))
-    height, width = img.shape[:2]
-    ext = re.findall('/format,(\w+)', option)
-    ext = '.' + ext[0] if ext else '.jpg'
 
     for cmd in option.split('/'):
         if cmd.startswith('resize'):
@@ -107,19 +100,21 @@ def version2(request, object_name, option):
             h = re.findall('h_(\d+)', cmd)
             w = int(w[-1]) if w else w
             h = int(h[-1]) if h else h
-            (height, width), img = resize(w, h, height, width, img)
+            img = resize(img, (w, h))
         elif cmd.startswith('crop'):
-            x, y, w, h = map(int, re.search('crop,x_(\d+),y_(\d+),w_(\d+),h_(\d+)', cmd).groups())
-            (height, width), img = crop(x, y, w, h, height=height, width=width, img=img)
+            x, y, w, h = [int(i) for i in re.search('crop,x_(\d+),y_(\d+),w_(\d+),h_(\d+)', cmd).groups()]
+            img = crop(img, (x, y, w, h))
         elif cmd.startswith('rotate'):
-            r = int(re.findall('rotate,(\d+)', cmd)[0])
-            img = rotate(r, img)
+            degree = int(re.findall('rotate,(\d+)', cmd)[0])
+            img = rotate(img, degree)
 
+    ext = re.findall('/format,(\w+)', option)
+    ext = '.' + ext[0] if ext else '.jpg'
     q = re.findall('/quality,[q,Q]_(\d+)', option)
     q = int(q[-1]) if q else 90
 
     try:
-        return HttpResponse(compress(q, ext, img), content_type='image/%s' % (ext[1:]))
+        return HttpResponse(compress(img, ext, q), content_type='image/%s' % (ext[1:]))
     except Exception, e:
         print e
         return HttpResponseRedirect('/%s/%s' % (bucket, object_name))
